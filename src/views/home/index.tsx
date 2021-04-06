46aa076c9eed89b52db43050eed849344d684fa0
N <N@N.N>
2021-04-06 14:57:52 +0700

    init nextjs

---
+++ src/views/home/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from 'react';
+
+export const Home = () => {
+	return (
+		<>
+			<Head>
+				<title>Chat</title>
+			</Head>
+			<div>Chat</div>
+		</>
+	);
+};
